9760accd3920618a379ffcb0f166ceace8840012
kavin2711 <kavinnt1@gmail.com>
2025-07-31 07:38:31 +0530

    trial2.py

--- trials.py
+++ trials.py
@@ -1,13 +1,24 @@
 def print_board():
-    counter=1
+    counter=0
     for row in range(3):
         print("+-------"*3 + "+")
         print("|       "*3 + "|" )
         for column in range (3):
-            print("|   "+ str(counter) + "   ", end="")
+            print("|   "+ str(board[counter]) + "   ", end="")
             counter+=1
             
         print("|")
         print("|       "*3 + "|" )
         
     print("+-------"*3 + "+")
+board=[1,2,3,4,5,6,7,8,9]
+
+def computer_move():
+    import random
+    move = random.randint(1, 9)
+    if move in board:
+       board[move-1]="o"
+    else:
+        computer_move()
+
+       
